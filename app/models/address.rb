f39354bd379c8083e2528eaf2d90a00e635a2f1c
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-02-15 13:32:37 -0300

    feat(model): add relations, nested attrs and validates to models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :municipe
+
+  validates :zip, :street, :complement, :neighborhood, :city, :uf, presence: true
 end
--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,2 +1,7 @@
 class Municipe < ApplicationRecord
+    has_one :address
+    accepts_nested_attributes_for :address
+
+    validates :full_name, :cpf, :phone, presence: true
+    validates :cpf, uniqueness: true
 end
